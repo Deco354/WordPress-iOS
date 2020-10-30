dcd2f3045bcc71b717bee7d65376fa809b062c6d
Declan McKenna <deco354@gmail.com>
2020-10-30 20:57:31 +0100

    Remove redundant hash functions
    These can now be found within SHAHasher.m

--- WordPress/Classes/Models/AbstractPost+HashHelpers.m
+++ WordPress/Classes/Models/AbstractPost+HashHelpers.m
@@ -44,65 +44,8 @@
     return [SHAHasher combineHashes:finalHashes];
 }
 
-
 - (NSArray<NSData *> *)additionalContentHashes {
     return @[];
-}
-
-#pragma mark - SHA256 calculations
-
-- (NSData *)hashForStringArray:(NSArray *) array {
-    NSString *joinedArrayString = [array componentsJoinedByString:@""];
-    return [SHAHasher hashForString:joinedArrayString];
-}
-
-- (NSData *)hashForString:(NSString *) string {
-    if (!string) {
-        return [[NSMutableData dataWithLength:CC_SHA256_DIGEST_LENGTH] copy];
-    }
-
-    NSData *encodedBytes = [string dataUsingEncoding:NSUTF8StringEncoding];
-    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
-
-    CC_SHA256(encodedBytes.bytes, (CC_LONG)encodedBytes.length, digest);
-
-    return [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
-}
-
-- (NSData *)hashForNSInteger:(NSInteger)integer {
-    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
-
-    CC_SHA256(&integer, sizeof(integer), digest);
-
-    return [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
-}
-
-- (NSData *)hashForDouble:(double)dbl {
-    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
-
-    CC_SHA256(&dbl, sizeof(dbl), digest);
-
-    return [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
-}
-
-- (NSData *)hashForBool:(BOOL)boolean {
-    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
-
-    CC_SHA256(&boolean, sizeof(boolean), digest);
-
-    return [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
-}
-
-- (NSString *)sha256StringFromData:(NSData *)data {
-    NSMutableString *mutableString = [NSMutableString string];
-
-    const char *hashBytes = [data bytes];
-
-    for (int i = 0; i < data.length; i++) {
-        [mutableString appendFormat:@"%02.2hhx", hashBytes[i]];
-    }
-
-    return [mutableString copy];
 }
 
 @end
